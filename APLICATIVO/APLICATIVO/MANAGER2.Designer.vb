af33626e04e4ef32a27c53349370cde4be7c8be4
santi129123 <santi_29_02@hotmail.com>
2016-09-05 17:15:40 -0500

    Add files via upload

---
+++ APLICATIVO/APLICATIVO/MANAGER2.Designer.vb
@@ -0,0 +1,417 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MANAGER2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MANAGER2))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.COMPRAR1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.VERINFO1 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.VERINFO2 = New System.Windows.Forms.Button()
+        Me.COMPRAR2 = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.VERINFO3 = New System.Windows.Forms.Button()
+        Me.COMPRAR3 = New System.Windows.Forms.Button()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.TOTAL = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.AÑO = New System.Windows.Forms.Label()
+        Me.CANCION = New System.Windows.Forms.Label()
+        Me.NOMBRE = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-2, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(143, 150)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.UseWaitCursor = True
+        '
+        'COMPRAR1
+        '
+        Me.COMPRAR1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.COMPRAR1.Location = New System.Drawing.Point(305, 18)
+        Me.COMPRAR1.Name = "COMPRAR1"
+        Me.COMPRAR1.Size = New System.Drawing.Size(82, 23)
+        Me.COMPRAR1.TabIndex = 4
+        Me.COMPRAR1.Text = "COMPRAR"
+        Me.COMPRAR1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.VERINFO1)
+        Me.Panel1.Controls.Add(Me.COMPRAR1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 188)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(399, 56)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 28)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Bruno Mars"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(25, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "just the way you are"
+        '
+        'VERINFO1
+        '
+        Me.VERINFO1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.VERINFO1.Location = New System.Drawing.Point(204, 18)
+        Me.VERINFO1.Name = "VERINFO1"
+        Me.VERINFO1.Size = New System.Drawing.Size(82, 23)
+        Me.VERINFO1.TabIndex = 5
+        Me.VERINFO1.Text = "VER INFO"
+        Me.VERINFO1.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackgroundImage = CType(resources.GetObject("Panel2.BackgroundImage"), System.Drawing.Image)
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.VERINFO2)
+        Me.Panel2.Controls.Add(Me.COMPRAR2)
+        Me.Panel2.Location = New System.Drawing.Point(12, 250)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(399, 56)
+        Me.Panel2.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(27, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Don Tetto"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 17)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "no digas lo siento"
+        '
+        'VERINFO2
+        '
+        Me.VERINFO2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.VERINFO2.Location = New System.Drawing.Point(204, 17)
+        Me.VERINFO2.Name = "VERINFO2"
+        Me.VERINFO2.Size = New System.Drawing.Size(82, 23)
+        Me.VERINFO2.TabIndex = 6
+        Me.VERINFO2.Text = "VER INFO"
+        Me.VERINFO2.UseVisualStyleBackColor = True
+        '
+        'COMPRAR2
+        '
+        Me.COMPRAR2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.COMPRAR2.Location = New System.Drawing.Point(305, 17)
+        Me.COMPRAR2.Name = "COMPRAR2"
+        Me.COMPRAR2.Size = New System.Drawing.Size(82, 23)
+        Me.COMPRAR2.TabIndex = 5
+        Me.COMPRAR2.Text = "COMPRAR"
+        Me.COMPRAR2.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackgroundImage = CType(resources.GetObject("Panel3.BackgroundImage"), System.Drawing.Image)
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel3.Controls.Add(Me.Label7)
+        Me.Panel3.Controls.Add(Me.Label8)
+        Me.Panel3.Controls.Add(Me.VERINFO3)
+        Me.Panel3.Controls.Add(Me.COMPRAR3)
+        Me.Panel3.Location = New System.Drawing.Point(12, 312)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(399, 56)
+        Me.Panel3.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(25, 29)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Reik"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(25, 16)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(57, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "inolvidable"
+        '
+        'VERINFO3
+        '
+        Me.VERINFO3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.VERINFO3.Location = New System.Drawing.Point(204, 16)
+        Me.VERINFO3.Name = "VERINFO3"
+        Me.VERINFO3.Size = New System.Drawing.Size(82, 23)
+        Me.VERINFO3.TabIndex = 6
+        Me.VERINFO3.Text = "VER INFO"
+        Me.VERINFO3.UseVisualStyleBackColor = True
+        '
+        'COMPRAR3
+        '
+        Me.COMPRAR3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.COMPRAR3.Location = New System.Drawing.Point(305, 16)
+        Me.COMPRAR3.Name = "COMPRAR3"
+        Me.COMPRAR3.Size = New System.Drawing.Size(82, 23)
+        Me.COMPRAR3.TabIndex = 5
+        Me.COMPRAR3.Text = "COMPRAR"
+        Me.COMPRAR3.UseVisualStyleBackColor = True
+        '
+        'Panel4
+        '
+        Me.Panel4.BackgroundImage = CType(resources.GetObject("Panel4.BackgroundImage"), System.Drawing.Image)
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel4.Controls.Add(Me.TOTAL)
+        Me.Panel4.Controls.Add(Me.Label2)
+        Me.Panel4.Location = New System.Drawing.Point(12, 374)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(399, 56)
+        Me.Panel4.TabIndex = 10
+        '
+        'TOTAL
+        '
+        Me.TOTAL.AutoSize = True
+        Me.TOTAL.Location = New System.Drawing.Point(247, 24)
+        Me.TOTAL.Name = "TOTAL"
+        Me.TOTAL.Size = New System.Drawing.Size(0, 13)
+        Me.TOTAL.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 18)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Total"
+        '
+        'Panel5
+        '
+        Me.Panel5.BackgroundImage = CType(resources.GetObject("Panel5.BackgroundImage"), System.Drawing.Image)
+        Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel5.Controls.Add(Me.Button1)
+        Me.Panel5.Controls.Add(Me.AÑO)
+        Me.Panel5.Controls.Add(Me.CANCION)
+        Me.Panel5.Controls.Add(Me.NOMBRE)
+        Me.Panel5.Controls.Add(Me.Label1)
+        Me.Panel5.Controls.Add(Me.Label10)
+        Me.Panel5.Controls.Add(Me.Label9)
+        Me.Panel5.Controls.Add(Me.PictureBox1)
+        Me.Panel5.Location = New System.Drawing.Point(12, 12)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(399, 128)
+        Me.Panel5.TabIndex = 11
+        Me.Panel5.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(367, -2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "X"
+        Me.Button1.UseMnemonic = False
+        Me.Button1.UseVisualStyleBackColor = False
+        Me.Button1.UseWaitCursor = True
+        Me.Button1.Visible = False
+        '
+        'AÑO
+        '
+        Me.AÑO.AutoSize = True
+        Me.AÑO.Location = New System.Drawing.Point(250, 103)
+        Me.AÑO.Name = "AÑO"
+        Me.AÑO.Size = New System.Drawing.Size(0, 13)
+        Me.AÑO.TabIndex = 8
+        Me.AÑO.UseWaitCursor = True
+        '
+        'CANCION
+        '
+        Me.CANCION.AutoSize = True
+        Me.CANCION.Location = New System.Drawing.Point(250, 59)
+        Me.CANCION.Name = "CANCION"
+        Me.CANCION.Size = New System.Drawing.Size(0, 13)
+        Me.CANCION.TabIndex = 7
+        Me.CANCION.UseWaitCursor = True
+        '
+        'NOMBRE
+        '
+        Me.NOMBRE.AutoSize = True
+        Me.NOMBRE.Location = New System.Drawing.Point(250, 19)
+        Me.NOMBRE.Name = "NOMBRE"
+        Me.NOMBRE.Size = New System.Drawing.Size(0, 13)
+        Me.NOMBRE.TabIndex = 6
+        Me.NOMBRE.UseWaitCursor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(167, 103)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "AÑO"
+        Me.Label1.UseWaitCursor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(164, 60)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(55, 13)
+        Me.Label10.TabIndex = 4
+        Me.Label10.Text = "CANCION"
+        Me.Label10.UseWaitCursor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(164, 19)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(57, 13)
+        Me.Label9.TabIndex = 3
+        Me.Label9.Text = "NOMBRE:"
+        Me.Label9.UseWaitCursor = True
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(12, 146)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(146, 44)
+        Me.AxWindowsMediaPlayer1.TabIndex = 12
+        '
+        'MANAGER2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(423, 447)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "MANAGER2"
+        Me.Text = "MANAGER2"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents COMPRAR1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents VERINFO1 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents VERINFO2 As Button
+    Friend WithEvents COMPRAR2 As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents VERINFO3 As Button
+    Friend WithEvents COMPRAR3 As Button
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TOTAL As Label
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents AÑO As Label
+    Friend WithEvents CANCION As Label
+    Friend WithEvents NOMBRE As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ APLICATIVO/APLICATIVO/MANAGER2.vb
@@ -0,0 +1,48 @@
+﻿Public Class MANAGER2
+    Private precio As Integer
+    Private Sub DomainUpDown1_SelectedItemChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub COMPRAR1_Click(sender As Object, e As EventArgs) Handles COMPRAR1.Click
+        precio = 250
+        TOTAL.Text = precio.ToString("$ ##0,00")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub COMPRAR2_Click(sender As Object, e As EventArgs) Handles COMPRAR2.Click
+        precio = 150
+        TOTAL.Text = precio.ToString("$ ##0,00")
+    End Sub
+
+    Private Sub COMPRAR3_Click(sender As Object, e As EventArgs) Handles COMPRAR3.Click
+        precio = 300
+        TOTAL.Text = precio.ToString("$ ##0,00")
+    End Sub
+
+    Private Sub VERINFO1_Click(sender As Object, e As EventArgs) Handles VERINFO1.Click
+        NOMBRE.Text = "just the way you are"
+        CANCION.Text = "Bruo Mars"
+        AÑO.Text = "2014"
+        Me.AxWindowsMediaPlayer1.URL = "E:\musica\bruno.mp3"
+
+
+    End Sub
+
+    Private Sub VERINFO2_Click(sender As Object, e As EventArgs) Handles VERINFO2.Click
+        NOMBRE.Text = "no digas lo siento"
+        CANCION.Text = "Don tetto"
+        AÑO.Text = "2010"
+        Me.AxWindowsMediaPlayer1.URL = "E:\musica\dontetto.mp3"
+    End Sub
+
+    Private Sub VERINFO3_Click(sender As Object, e As EventArgs) Handles VERINFO3.Click
+        NOMBRE.Text = "inolvidable"
+        CANCION.Text = "Reik"
+        AÑO.Text = "2010"
+        Me.AxWindowsMediaPlayer1.URL = "E:\musica\reik.mp3"
+    End Sub
+End Class
